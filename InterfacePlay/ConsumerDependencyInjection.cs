2559376927f39511636e38f2f051d4627c8fb820
Serge Gagnon <serge.gagnon.1@live.ca>
2019-04-02 19:49:59 -0400

    Ajout classe ConsumerDependencyInjection

---
+++ InterfacePlay/ConsumerDependencyInjection.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+
+namespace InterfacePlay
+{
+    public class ConsumerDependencyInjection
+    {
+        private const string request = "http://127.0.0.1/queryThat?option=this";
+
+        private IRestClient restClient;
+        private ParameterSet _parameterSet;
+
+        public ConsumerDependencyInjection(ParameterSet parameterSet, IRestClient restClient)
+        {
+            _parameterSet = parameterSet;
+            InitializeComponent(restClient);
+        }
+
+        public void SendRequest()
+        {
+            var restRequest = new RestRequest()
+            {
+                Request = request
+            };
+            restClient.AddRestRequest(restRequest);
+        }
+
+        public HttpStatusCode HttpStatusCode { get; set; } = HttpStatusCode.BadRequest;
+
+        public string RestClientName { get; set; }
+
+        private void InitializeComponent(IRestClient restClient)
+        {
+
+            restClient.RestServerResponded += (sender, restEvent) =>
+            {
+                HttpStatusCode = restEvent.HttpStatusCode;
+                RestClientName = restEvent.RestClientName;
+            };
+        }
+    }
+
+}
---
+++ InterfacePlay/InterfacePlay.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacePlay
+{
+    public class InterfacePlay
+    {
+        public InterfacePlay()
+        {
+            InjectDependency();
+        }
+
+        private void InjectDependency()
+        {
+            var parameterSet = new ParameterSet();
+            var restClient = new RestClientFake();
+            var consumerDepInjected = new ConsumerDependencyInjection(parameterSet, restClient);
+        }
+    }
+}
